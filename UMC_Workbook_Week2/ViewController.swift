2548c48ee55c2aa545b323db4096915780045aa7
jangjoonmo <jjmo97@ajou.ac.kr>
2023-04-05 00:37:48 +0900

    Initial Commit

---
+++ UMC_Workbook_Week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_Workbook_Week2
+//
+//  Created by 장준모 on 2023/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
