f3b1d007fbec10caf071193ea29d804694e10b05
jangjoonmo <jjmo97@ajou.ac.kr>
2023-04-07 01:37:32 +0900

    UMC 2주차 Layout 실습

---
+++ UMC_Workbook_Week2/TestVC.swift
@@ -0,0 +1,34 @@
+//
+//  TestVC.swift
+//  UMC_Workbook_Week2
+//
+//  Created by 장준모 on 2023/04/07.
+//
+
+import UIKit
+
+class TestVC: UIViewController {
+
+    @IBOutlet var image1: UIImageView!
+    @IBOutlet var image2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
